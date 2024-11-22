a9c129f3a573a2d4d15de65987d4e470a4d77345
Murilo Francisco Matos <murilofm.social@gmail.com>
2024-11-22 18:18:08 -0300

    add e config prettier

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Você está evoluindo em programação meu caro leitor de comentários, não desista!</h1>
+  return (
+    <h1>
+      Você está evoluindo em programação meu caro leitor de comentários, não
+      desista!
+    </h1>
+  );
 }
 
 export default Home;
